6ca6c1345672aa1834ec4c2c3183b0d054e2a7dc
matheusmosca <matheusmoscag@gmail.com>
2020-08-19 11:50:47 -0300

    Initial commit

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+# Front-end react app
---
+++ frontend/src/pages/home/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../assets/styles/global.scss';
+
+export function Home() {
+  return (
+    <div>Hello world</div>
+  );
+}
