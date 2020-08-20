2f992d5db748f778e74b79169d8bb1e9afb19177
matheusmosca <matheusmoscag@gmail.com>
2020-08-19 22:20:18 -0300

    Feat: Sign up working with the backend

--- frontend/src/components/forms/signupform/index.js
+++ frontend/src/components/forms/signupform/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import { api } from '../../../services/api';
 
 import '../../../assets/styles/global.scss';
 import './signUpForm.scss';
@@ -8,21 +10,45 @@ import { MainButton } from "../../layout/mainButton";
 import { MainInput } from "../../layout/mainInput";
 
 export function SignUpForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+
+  async function handleSignUpForm(e) {
+    e.preventDefault();
+    if (password !== repeatPassword) {
+      alert("As senhas não coincidem. Tente novamente.")
+      return;
+    }
+    try {
+      const response = await api.post('/Users', {
+        username,
+        password
+      });
+
+      alert("Sua conta foi criada com sucesso")
+      return response
+    } catch(error) {
+      console.log(error)
+      return error.response;
+    }
+  }
+
   return (
     <div className="signup-form-container">
-      <form action="" className="signup-form-content">
+      <form onSubmit={ (e) => handleSignUpForm(e) } action="" className="signup-form-content">
         <h4>Criar conta</h4>
-        <MainInput required={ true } inputType="text" labelName="Username"/>
-        <MainInput required={ true } inputType="password" labelName="Senha"/>
-        <MainInput required={ true } inputType="password" labelName="Repita a senha"/>
+        <MainInput required={ true } saveInputValue={ setUsername } inputType="text" labelName="Username"/>
+        <MainInput required={ true } saveInputValue={ setPassword } inputType="password" labelName="Senha"/>
+        <MainInput required={ true } saveInputValue={ setRepeatPassword } inputType="password" labelName="Repita a senha"/>
         <div className="center-text">
           <div className="redirect-text">
             Já tem um cadastro? <Link to="/signin"><span>Logar</span></Link>
           </div>
         </div>
-        {/* <Link to="/"> */}
+        <Link to="/signin">
           <MainButton buttonText="Criar conta"/>
-        {/* </Link> */}
+        </Link>
       </form>
     </div>
   )
--- frontend/src/components/layout/mainInput/index.js
+++ frontend/src/components/layout/mainInput/index.js
@@ -3,11 +3,11 @@ import React from 'react';
 import '../../../assets/styles/global.scss';
 import './mainInput.scss';
 
-export function MainInput({ inputType, labelName, required }) {
+export function MainInput({ inputType, labelName, saveInputValue, required }) {
   return (
     <div className="input-container">
       <label>{ labelName }</label>
-      <input required={ required } className="form-input" type={ inputType }/>
+      <input required={ required } onChange={ (e) => saveInputValue(e.target.value) } className="form-input" type={ inputType }/>
     </div>
   )
 }
