9015d7727df57641bc627f5618aaade99663963f
matheusmosca <matheusmoscag@gmail.com>
2020-08-22 11:48:20 -0300

    Feat: Exibição de histórico de vendas.

---
+++ frontend/src/pages/salesPage/index.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+
+import MaterialTable from 'material-table'
+
+import { api } from '../../services/api';
+
+import '../../assets/styles/global.scss';
+
+import './salesPage.scss';
+import { DashboardBanner } from '../../components/layout/dashboardBanner';
+import { useAuth } from '../../contexts/Auth';
+
+export function SalesPage() {
+  const { authorizationBearer } = useAuth();
+  const [sales, setSales] = useState();
+
+  useEffect(() => {
+    const fetchSales = async () => {
+      try {
+        const response = await api.get('/Sales', { 
+          headers: { Authorization: authorizationBearer() } 
+        });
+        const data = await response.data;
+
+        const sales = data.map((sale) => {
+          const saleData = {
+            value: sale.value,
+            amount: sale.amount,
+            productName: sale.product.name
+          }
+          return saleData;
+        });
+        setSales([...sales]);
+
+      } catch(error) {
+        console.log(error);
+        return error.response;
+      }
+    }
+
+    fetchSales();
+  }, [authorizationBearer]);
+
+  console.log(sales)
+
+  return (
+    <section className="dashboard-page-container">
+      <DashboardBanner />
+      <div className="dashboard-content">
+        <div className="dashboard-table">
+          <MaterialTable
+            columns={[
+              { title: 'Produto', field: 'productName' },
+              { title: 'Quantidade', field: 'amount', type: 'numeric' },
+              { title: 'Valor', field: 'value', type: 'numeric' },
+            ]}
+            data={ sales }
+            options={{
+              // search: false,
+              exportButton: true
+            }}
+            title="Histórico de vendas"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
--- frontend/src/routes/AuthRoutes.js
+++ frontend/src/routes/AuthRoutes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import { ProductsPage } from '../pages/productsPage';
+import { SalesPage } from '../pages/salesPage';
 import { AuthProvider } from '../contexts/Auth';
 
 export function AuthRoutes() {
   return (
     <AuthProvider>
       <Route exact path="/" component={ ProductsPage }/>
+      <Route exact path="/sales" component={ SalesPage }/>
     </AuthProvider>
   )
 }
